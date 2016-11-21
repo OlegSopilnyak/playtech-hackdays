3f5c8622c90386a8ae8f31ae6745b530cadbf4e6
OlegSopilnyak <oleg.sopilnyak@gmail.com>
2016-11-21 19:11:48 +0200

    Made it is ready to use modules scanner service on client and POC for operations counter (front-end)

--- hm-openbet-web/src/main/webapp/WEB-INF/openbet/javascript/healthServices.js
+++ hm-openbet-web/src/main/webapp/WEB-INF/openbet/javascript/healthServices.js
@@ -11,8 +11,7 @@ var HealthScanner = (function () {
     var instance;
 
     function createInstance() {
-        var service = new HealthScannerService();
-        return service;
+        return new HealthScannerService();
     }
 
     return {
@@ -35,7 +34,7 @@ function HealthScannerModule() {
     this.appId = "RegisteredModulesScanner";
     this.ver = "0.01";
     this.descr = "Service to scan registered modules and manage them";
-};
+}
 HealthScannerModule.prototype = Object.create(Module.prototype);
 HealthScannerModule.prototype.constructor = HealthScannerModule;
 HealthScannerModule.prototype.systemId = function () {return this.sysId;};
@@ -51,30 +50,21 @@ HealthScannerModule.prototype.description = function () {return this.descr;};
 function HealthScannerService() {
     MonitoredService.call(this, new HealthScannerModule());
     var self = this;
+
     var DELAY_PATH = "health.monitor.service.external.heartbeat.delay";
     var delay = 2000;
-    this.configuration[DELAY_PATH] = "2000";
+
+    var PING_URL_PATH = "health.monitor.service.external.heartbeat.ping.url";
+    var pingUrl = "module/ping";
 
     var timerId = null;
     var firstTime = true;
     var registeredModules = [this];
 
-    /**
-     * Method called when configuration was changed by server
-     *
-     * @private
-     */
-    this._configurationChanged = function(){
-        var newDelay = +this.configuration[DELAY_PATH];
-        if (isNaN(newDelay)) {
-            // restore value by default
-            delay = 2000;
-            this.configuration[DELAY_PATH] = "2000";
-        }else {
-            // changing the value of delay
-            delay = newDelay;
-        }
-    };
+    // define external configuration parameters
+    this.configuration[DELAY_PATH] = "2000";
+    this.configuration[PING_URL_PATH] = "module/ping";
+
     /**
      * To register module for scan
      *
@@ -101,123 +91,222 @@ function HealthScannerService() {
         }
     };
     /**
-     * Exchange data with server
-     * @param snapshot snapshot of module state
-     * @returns {Array} changed configurations
-     */
-    function ping(snapshot, onSuccess, onFail, logger) {
-        var json = JSON.stringify(snapshot);
-        var updatedConfiguration = [];
-        logger.associateAction("ping","Pinging remote server");
-        logger.actionBegin();
-        logger.print("Send ping request for ", snapshot.module.key());
-        console.log("Sending: "+json);
-        $.ajax({
-            url: 'module/ping',
-            data: json,
-            contentType: "application/json",
-            dataType: 'json',
-            type: 'POST'
-        }).then(onSuccess, onFail);
-    }
-    /**
      * To start scan
      */
     this.start = function () {
         var self = this;
-        var logger = this.createOutputDevice("log");
-        console.log("Starting scaner...");
-        logger.print("Starting scaner...");
+        var logger = self.createOutputDevice("log");
+console.log("Starting modules scanner...");
+        logger.print("Starting scanner...");
+        firstTime = true;
         function processRegistered() {
             logger = self.createOutputDevice("log");
             logger.print("Processing ",registeredModules.length," module(s) ...");
-            console.log("Processing "+registeredModules.length+" module(s) ...");
+// console.log("Processing "+registeredModules.length+" module(s) ...");
+            // iteration of registered modules array
             registeredModules
                 .filter(function (module) {
 
                     // logging begin
                     var moduleDescription = module.healthPK().description();
                     logger.associateAction("checkModule", "Checking the module "+ moduleDescription);
-                    console.log("Processing module : "+moduleDescription);
+console.log("Processing module : "+ moduleDescription);
                     logger.print("Processing module : ",moduleDescription);
                     // logging end
 
                     return firstTime || timerId != null;
                 })
-                .forEach(function (module, i, arr) {
+                .forEach(function (module) {
                     var moduleDescription = module.healthPK().description();
-                    var processSuccessPong = function (data) {
-                        if (data.length > 0) {
-
-                            console.log("updating module " + moduleDescription);
-                            logger.print("updating module ", moduleDescription);
-                            module.configurationChanged(data);
-                        }
-                        console.log("Check is module works well active:" + module.active());
-                        logger.print("Check is module works well active:", module.active());
-                        if (!module.active()) {
-                            console.log("Module restarting....");
-                            logger.print("Module restarting....");
-                            module.restart();
-                        }
-                        logger.print("Module ", moduleDescription, " verified well.");
-                        logger.actionSuccess();
-                    };
-                    var processFail = function (req, status, err) {
-                        console.log("For module "+moduleDescription+" Received error "+err);
-                        logger.print("For module ",moduleDescription," Received error ",err);
-                        logger.actionFail();
-                    }
+                    // start the action "checkModule"
                     logger.actionBegin();
                     try {
 
-                        console.log("grabbing module's snapshot... ");
+// console.log("grabbing module's snapshot... ");
                         logger.print("grabbing module's snapshot... ");
-
+                        // grab module's snapshot
                         var snapshot = module.snapshot();
 
-                        console.log("grabed module's snapshot... ");
-                        logger.print("grabed module's snapshot... ");
+// console.log("grabbed module's snapshot... ");
+                        logger.print("grabbed module's snapshot... ");
 
+                        // send POST request and register response's handlers
                         ping(snapshot, processSuccessPong, processFail, logger);
                         snapshot = null;
                     }catch (err){
-                        console.log("Check of ["+moduleDescription+"] interrupted", err)
+// console.log("Check of ["+moduleDescription+"] interrupted", err);
                         logger.print("Check interrupted", err);
+                        // action for particular module is failed (bad request)
+                        logger.actionFail();
+                    }
+
+                    //////////////////////////////////////////////////////////
+                    ///////////  private functions and methods   /////////////
+                    //////////////////////////////////////////////////////////
+                    /**
+                     * Callback function for module's ping request
+                     *
+                     * @param data received data (module's configuration updates (array of configurationItem(s)))
+                     */
+                    function processSuccessPong (data) {
+                        if (data.length > 0) {
+
+// console.log("updating module " + moduleDescription);
+                            logger.print("updating module ", moduleDescription);
+                            // updating module's configuration
+                            module.configurationChanged(data);
+
+// console.log("updated module " + moduleDescription);
+                            logger.print("updated module ", moduleDescription);
+                        }
+// console.log("Check is module works well active:" + module.active());
+                        logger.print("Check is module works well active:", module.active());
+                        if (!module.active()) {
+
+// console.log("Module restarting....");
+                            logger.print("Module restarting....");
+                            // restarting passive module
+                            module.restart();
+
+// console.log("Module restarted.");
+                            logger.print("Module restarted.");
+                        }
+                        logger.print("Module ", moduleDescription, " verified well.");
+console.log("Module " + moduleDescription + " verified well.");
+                        // action for particular module is success
+                        logger.actionSuccess();
+                    }
+                    /**
+                     * Process the ping request's fail
+                     *
+                     * @param req request instance
+                     * @param status status of request
+                     * @param err error obkect
+                     */
+                    function processFail(req, status, err) {
+// console.log("For module "+moduleDescription+" Received error "+err);
+                        logger.print("For module ",moduleDescription," Received error ",err);
+console.log("Module " + moduleDescription + " verified fail.");
+                        // action for particular module is failed (bad response)
                         logger.actionFail();
                     }
                 }
             );
             if (firstTime || timerId != null) {
-                console.log("Next iteration in "+delay+" ms.");
+// console.log("Next iteration in "+delay+" ms.");
                 logger.print("Next iteration in "+delay+" ms.");
+                // start the new iteration
                 timerId = setTimeout(processRegistered, delay);
             }else{
-                console.log("Service stopped.");
+                // service finished
+console.log("Service-scanner stopped.");
                 logger.print("Service stopped.");
             }
         }
         processRegistered();
         firstTime = false;
-    }
+    };
     /**
      * To check is service works
      * @returns {boolean} true if works
      */
-    this.isWorks = function () {return firstTime || timerId != null;}
+    this.isWorks = function () {return firstTime || timerId != null;};
     /**
      * To stop scan
      */
     this.stop = function (){
         if (timerId){
+            // forced service shutdown
             clearTimeout(timerId);
             timerId = null;
             firstTime = true;
         }
-    }
-};
+    };
+
+    //////////////////////////////////////////////////////////
+    ///////////  private functions and methods   /////////////
+    //////////////////////////////////////////////////////////
+    /**
+     * Method called when configuration was changed by server
+     *
+     * @private
+     */
+    this._configurationChanged = function(){
+        var logger = this.createOutputDevice("log");
+        logger.associateAction("configurationChanged", "Received notification about module's configuration changes.");
+        logger.actionBegin();
+        // processing the "delay" attribute
+        checkDelayChangeConfiguration(logger);
+        // processing "pingUrl" attribute
+        checkPingUrlChangeConfiguration(logger);
+        logger.actionSuccess();
+    };
+
+    /**
+     * Exchange data with server
+     * @param snapshot snapshot of module state
+     * @returns {Array} changed configurations
+     */
+    /**
+     * Exchange data with server
+     *
+     * @param snapshot snapshot of module state
+     * @param onSuccess callback function for exchange's success
+     * @param onFail callback function for exchange's fail
+     * @param logger scanner-module's output logger
+     */
+    var ping = function(snapshot, onSuccess, onFail, logger) {
+        var json = JSON.stringify(snapshot);
+        logger.print("Send ping request for ", snapshot.module.key());
+// console.log("Sending: "+json);
+        $.ajax({
+            url: pingUrl,
+            data: json,
+            contentType: "application/json",
+            dataType: 'json',
+            type: 'POST'
+        }).then(onSuccess, onFail);
+    };
+
+    /**
+     * To check changes for "delay"
+     */
+    var checkDelayChangeConfiguration = function(logger) {
+        logger.print("Check changes for ", DELAY_PATH);
+        var newValue = +self.configuration[DELAY_PATH];
+        if (isNaN(newValue)) {
+            // restore value by default
+// console.log("Restore value to default.");
+            logger.print("Restore value to default.");
+            delay = 2000;
+            self.configuration[DELAY_PATH] = "2000";
+        } else {
+            logger.print("Change value of ",DELAY_PATH, " to ", newValue);
+// console.log("Change value to " + newValue);
+            // changing the value of delay
+            delay = newValue;
+        }
+    };
+
+    /**
+     * To check changes for "pingUrl"
+     */
+    var checkPingUrlChangeConfiguration = function(logger) {
+        logger.print("Check changes for ", PING_URL_PATH);
+        var newValue = self.configuration[PING_URL_PATH];
+        if (newValue) {
+            // changing the value of pingUrl
+            logger.print("Change value of ",PING_URL_PATH, " to ", newValue);
+// console.log("Change value to " + newValue);
+            pingUrl = newValue;
+        } else {
+            // do nothing
+// console.log("Do nothing.");
+        }
+    };
+}
 HealthScannerService.prototype = Object.create(MonitoredService.prototype);
-HealthScannerService.prototype.constructor = HealthScanner;
+HealthScannerService.prototype.constructor = HealthScannerService;
 HealthScannerService.prototype.active = function () {return this.isWorks();};
 HealthScannerService.prototype.restart = function () {this.stop(); this.start();};
 HealthScannerService.prototype._applyConfigurationChanges = function () {
--- hm-openbet-web/src/main/webapp/WEB-INF/openbet/javascript/monitor.js
+++ hm-openbet-web/src/main/webapp/WEB-INF/openbet/javascript/monitor.js
@@ -1,6 +1,151 @@
 var Monitor = function () {
+    /**
+ * The module of service
+ *
+ * @constructor
+ */
+function OperationsCountModule() {
+    Module.call(this, "HealthMonitor");
+    this.sysId = "openBetOperations";
+    this.appId = "operationsCounter";
+    this.ver = "0.01";
+    this.descr = "Service to count operations that suit search criteria";
+}
+OperationsCountModule.prototype = Object.create(Module.prototype);
+OperationsCountModule.prototype.constructor = OperationsCountModule;
+OperationsCountModule.prototype.systemId = function () {return this.sysId;};
+OperationsCountModule.prototype.applicationId = function () {return this.appId;};
+OperationsCountModule.prototype.version = function () {return this.ver;};
+OperationsCountModule.prototype.description = function () {return this.descr;};
+
+/**
+ * The service to calculate OpenBet operations suitable for the search criteria
+ *
+ * @constructor
+ */
+function OperationsCounterService(){
+    MonitoredService.call(this, new OperationsCountModule());
+    var self = this;
+    var DELAY_PATH = "health.monitor.service.external.ob.counter.delay";
+    var delay = 2000;
+    var BORDER_COLOR_PATH = "health.monitor.service.external.ob.counter.border.color";
+    var borderColor = "green";
+    var selected = 0;
+    // thread properties
+    var timerId = null;
+    var firstTime = true;
+
+    // define external configuration parameters
+    this.configuration[DELAY_PATH] = "2000";
+    this.configuration[BORDER_COLOR_PATH] = "green";
+
+    this.selectedBorderColor = ko.observable("1px dashed "+borderColor+"");
+    this.logger = self.createOutputDevice("log");
+
+    this.exchange = function (logger) {
+        try{
+            logger.print("Retrieving operations count ","by criteria : ",  ko.toJSON(criteria), " ...");
+// console.log("Using criteria : " + ko.toJSON(criteria) + " ...");
+// console.log("Retrieving operations count from server......");
+            self.selectedBorderColor("1px dashed yellow");
+            server.getOperationsCount(criteria, counterReceived);
+        }catch (err){
+            logger.print("Error ", err);
+            logger.actionFail();
+// console.log("Error", err);
+            self.selectedBorderColor("1px dotted yellow");
+        }
+        function counterReceived(data){
+            logger.print("Received ", data, " selected operations.");
+            operationsCount(data);
+            self.selectedBorderColor("1px dashed "+borderColor+"");
+            logger.actionSuccess();
+        }
+    };
+
+    this.isWorks = function () {return firstTime || timerId != null;};
+
+    this.start = function(){
+        firstTime = true;
+        this.logger.print(" Service-counter starting...");
+console.log("Starting the service-counter");
+        function processOperationsCounting(){
+            var logger = self.createOutputDevice("log");
+            logger.associateAction("getOperationsCount", "Count operations suitable for criteria.");
+            logger.actionBegin();
+            self.exchange(logger);
+            if (firstTime || timerId != null) {
+                self.logger.print("Next iteration in ", delay, " ms.");
+// console.log("Next iteration in "+delay+" ms.");
+                // start the new iteration
+                timerId = setTimeout(processOperationsCounting, delay);
+            }else{
+                // service finished
+                self.logger.print("Service-counter stopped.");
+console.log("Service-counter is stopped.");
+                // unregister module
+                HealthScanner.getInstance().unregister(this);
+            }
+        }
+        processOperationsCounting();
+        firstTime = false;
+        HealthScanner.getInstance().register(this);
+    };
+    this.stop = function (){
+        if (timerId){
+            // forced service shutdown
+            clearTimeout(timerId);
+            timerId = null;
+            firstTime = true;
+        }
+    };
+    this._configurationChanged = function(){
+        // processing the "delay" attribute
+        checkDelayChangeConfiguration(this.logger);
+        // processing "borderColor" attribute
+        checkBorderColorChangeConfiguration(this.logger);
+    };
+
+    var checkDelayChangeConfiguration = function(logger) {
+        var newValue = +self.configuration[DELAY_PATH];
+        if (isNaN(newValue)) {
+            // restore value by default
+// console.log("Restore value to default.");
+            logger.print("Restore value to default.");
+            delay = 2000;
+            self.configuration[DELAY_PATH] = "2000";
+        } else {
+            logger.print("Change value to ", newValue);
+// console.log("Change value to " + newValue);
+            // changing the value of delay
+            delay = newValue;
+        }
+    };
+    var checkBorderColorChangeConfiguration = function(logger) {
+        var newValue = self.configuration[BORDER_COLOR_PATH];
+        if (newValue) {
+            // changing the value of pingUrl
+            logger.print("Change value to ", newValue);
+// console.log("Change value to " + newValue);
+            borderColor = newValue;
+        } else {
+            // do nothing
+// console.log("Do nothing.");
+        }
+    };
+}
+OperationsCounterService.prototype = Object.create(MonitoredService.prototype);
+OperationsCounterService.prototype.constructor = OperationsCounterService;
+OperationsCounterService.prototype.active = function () {return this.isWorks();};
+OperationsCounterService.prototype.restart = function () {this.stop(); this.start();};
+OperationsCounterService.prototype._applyConfigurationChanges = function () {this._configurationChanged();};
+
+
     /* module to retrieve data from the server */
     var server = ServerFactory.getServer();
+
+    // the service to count selected operations
+    var counterService = new OperationsCounterService();
 
     /* the model */
     var model = {
@@ -24,7 +169,8 @@ var Monitor = function () {
 
     /* method to set the active page */
     var setActivePage = function (page) {
-        console.log("Setting active page to: " + page);
+        counterService.logger.print("Setting active page to: ", page);
+// console.log("Setting active page to: " + page);
         activePage(page);
     };
 
@@ -36,7 +182,8 @@ var Monitor = function () {
 
     /* sets the selected account */
     var setSelectedOperation = function (operation) {
-        console.log("Setting selected operation: " + operation);
+        counterService.logger.print("Setting selected operation: ", operation);
+// console.log("Setting selected operation: " + operation);
         model.selectedOperation(operation);
     };
 
@@ -45,15 +192,10 @@ var Monitor = function () {
         return operation === model.selectedOperation();
     };
 
-    /* process operations count received from server */
-    var processOperationsCount = function(data){
-        console.log("Operations count retrieved from server: " + ko.toJSON(data));
-        operationsCount(data);
-    }
-
     /* process operations array received from server */
     var processOperations = function (data) {
-        console.log("Operations data retrieved from server: " + ko.toJSON(data));
+        counterService.logger.print("Operations data retrieved from server.");
+// console.log("Operations data retrieved from server: " + ko.toJSON(data));
         // clear model for operations
         model.operations([]);
         //add operations to the model
@@ -65,39 +207,41 @@ var Monitor = function () {
     };
     /* process operation types received from server */
     var processTypes = function (data) {
-        console.log("Data retrieved from server: " + ko.toJSON(data));
+        counterService.logger.print("Operation types data retrieved from server.");
+// console.log("Data retrieved from server: " + ko.toJSON(data));
         // clear model for types
         model.types([]);
         //add received types to the model
         model.types.push({
-            value: "--- None ---", setOperationType: function () {
-                criteria.operationType(null);
-            }
+            value: "--- None ---",
+            setOperationType: function () {criteria.operationType(null);}
         });
 
         data.forEach(function (name) {
             model.types.push({
-                value: name, setOperationType: function () {
-                    criteria.operationType(name);
-                }
+                value: name,
+                setOperationType: function () {criteria.operationType(name);}
             });
         });
     };
     /* method to start retrieving the data from the server side and sets it in the model */
     var retrieveData = function () {
-        console.log("Using criteria : " + ko.toJSON(criteria) + " ...")
-        console.log("Retrieving operations data from server......");
+        counterService.logger.print("Retrieving operations data by criteria ", ko.toJSON(criteria));
+// console.log("Using criteria : " + ko.toJSON(criteria) + " ...");
+// console.log("Retrieving operations data from server......");
         server.getOperationsData(criteria, processOperations);
 
-        console.log("Retrieving operation types from server......");
+        counterService.logger.print("Retrieving operation types from server...");
+// console.log("Retrieving operation types from server......");
         server.getOperationTypes(processTypes);
     };
 
-    var retrieveCount = function(){
-        console.log("Using criteria : " + ko.toJSON(criteria) + " ...")
-        console.log("Retrieving operations count from server......");
-        server.getOperationsCount(criteria, processOperationsCount);
-    };
+    function retrieveCount(){
+        var logger = counterService.createOutputDevice("log");
+        logger.associateAction("getOperationsCount", "Count operations suitable for criteria.");
+        logger.actionBegin();
+        counterService.exchange(logger);
+    }
 
     var subscribeCriteria = function(){
         criteria.operationType.subscribe(retrieveCount);
@@ -109,11 +253,7 @@ var Monitor = function () {
 
     /* computed observable for title drop down text change */
     var typeSelected = ko.pureComputed(function () {
-        if (criteria.operationType() == null) {
-            return "select"
-        } else {
-            return criteria.operationType();
-        }
+        return criteria.operationType() ? criteria.operationType() : "select";
     });
 
     /* Add handler for datepicker */
@@ -171,6 +311,7 @@ var Monitor = function () {
 
         retrieveData();
         retrieveCount();
+        counterService.start();
 
         /* subscribe to criteria fields change */
         subscribeCriteria();
@@ -189,6 +330,7 @@ var Monitor = function () {
         model: model,
         operationsTypes: model.types,
         operationsCount: operationsCount,
+        selectedBorderStyle: counterService.selectedBorderColor,
         setActivePage: setActivePage,
         isActivePage: isActivePage,
         setSelectedOperation: setSelectedOperation,
