e82149b90aba32046561656aeb306a3700d4f673
Oleg Sopilnyak <oleg.sopilnyak@gmail.com>
2019-02-28 18:31:21 +0200

    Covered health-modules-service

--- modularity/core/src/main/java/oleg/sopilnyak/service/registry/impl/HealthModuleService.java
+++ modularity/core/src/main/java/oleg/sopilnyak/service/registry/impl/HealthModuleService.java
@@ -29,206 +29,208 @@ import java.util.stream.Stream;
  */
 @Slf4j
 public class HealthModuleService extends RegistryModulesIteratorAdapter implements ModulesRegistryService {
-	private static final String ACTIVITY_LABEL = "HealthCheck";
-	// future of scheduled activities
-	private volatile ScheduledFuture runnerFuture;
-	private volatile long delay = DELAY_DEFAULT;
-	private volatile String[] ignoredModules = IGNORE_MODULE_DEFAULT.split(",");
-	// the map of registered modules
-	private final Map<String, Module> modules = new ConcurrentHashMap<>();
+    private static final String ACTIVITY_LABEL = "HealthCheck";
+    // future of scheduled activities
+    private volatile ScheduledFuture runnerFuture;
+    private volatile long delay = DELAY_DEFAULT;
+    private volatile String[] ignoredModules = IGNORE_MODULE_DEFAULT.split(",");
+    // the map of registered modules
+    private final Map<String, Module> modules = new ConcurrentHashMap<>();
 
-	@Autowired
-	private ModuleMetricStorage metricStorage;
+    @Autowired
+    private ModuleMetricStorage metricStorage;
 
-	public HealthModuleService() {
-		super.registry = this;
-		moduleConfiguration.put(delayName(), new VariableItemDto(DELAY_NAME, DELAY_DEFAULT));
-		moduleConfiguration.put(ignoreModulesName(), new VariableItemDto(IGNORE_MODULE_NAME, IGNORE_MODULE_DEFAULT));
-	}
+    public HealthModuleService() {
+        super.registry = this;
+        moduleConfiguration.put(delayName(), new VariableItemDto(DELAY_NAME, DELAY_DEFAULT));
+        moduleConfiguration.put(ignoreModulesName(), new VariableItemDto(IGNORE_MODULE_NAME, IGNORE_MODULE_DEFAULT));
+    }
 
-	/**
-	 * To add module to registry
-	 *
-	 * @param module to add
-	 */
-	@Override
-	public void add(final Module module) {
-		assert module != null;
-		if (module != this) {
-			// activate main module action
-			activateMainModuleAction();
+    /**
+     * To add module to registry
+     *
+     * @param module to add
+     */
+    @Override
+    public void add(final Module module) {
+        assert module != null;
+        if (module != this) {
+            // activate main module action
+            activateMainModuleAction();
 
-			log.debug("Registering '{}'", module.primaryKey());
-			modules.putIfAbsent(module.primaryKey(), module);
-		}
-	}
+            log.debug("Registering '{}'", module.primaryKey());
+            modules.putIfAbsent(module.primaryKey(), module);
+        }
+    }
 
-	/**
-	 * To remove module from registry
-	 *
-	 * @param module to remove
-	 */
-	@Override
-	public void remove(final Module module) {
-		assert module != null;
-		if (module != this) {
-			// activate main module action
-			activateMainModuleAction();
+    /**
+     * To remove module from registry
+     *
+     * @param module to remove
+     */
+    @Override
+    public void remove(final Module module) {
+        assert module != null;
+        if (module != this) {
+            // activate main module action
+            activateMainModuleAction();
 
-			log.debug("Unregistering '{}'", module.primaryKey());
-			modules.remove(module.primaryKey());
-		}
-	}
+            log.debug("Unregistering '{}'", module.primaryKey());
+            modules.remove(module.primaryKey());
+        }
+    }
 
-	/**
-	 * To get collection of registered modules
-	 *
-	 * @return collection of registered modules
-	 */
-	@Override
-	public Collection<Module> registered() {
-		return new LinkedHashSet<>(modules.values());
-	}
+    /**
+     * To get collection of registered modules
+     *
+     * @return collection of registered modules
+     */
+    @Override
+    public Collection<Module> registered() {
+        return new LinkedHashSet<>(modules.values());
+    }
 
-	/**
-	 * To get registered module by module's primary key
-	 *
-	 * @param modulePK primary key
-	 * @return module or null if not registered
-	 */
-	@Override
-	public Module getRegistered(String modulePK) {
-		return StringUtils.isEmpty(modulePK) ? null : modules.get(modulePK);
-	}
+    /**
+     * To get registered module by module's primary key
+     *
+     * @param modulePK primary key
+     * @return module or null if not registered
+     */
+    @Override
+    public Module getRegistered(String modulePK) {
+        return StringUtils.isEmpty(modulePK) ? null : modules.get(modulePK);
+    }
 
-	/**
-	 * To get registered module by module instance
-	 *
-	 * @param module module instance
-	 * @return module or null if not registered
-	 */
-	@Override
-	public Module getRegistered(ModuleBasics module) {
-		assert module != null : "Module couldn't be null";
-		return getRegistered(module.primaryKey());
-	}
+    /**
+     * To get registered module by module instance
+     *
+     * @param module module instance
+     * @return module or null if not registered
+     */
+    @Override
+    public Module getRegistered(ModuleBasics module) {
+        assert module != null : "Module couldn't be null";
+        return getRegistered(module.primaryKey());
+    }
 
-	/**
-	 * Allocate module's resources and get module ready to work
-	 */
-	@Override
-	protected void initAsService() {
-		if (log.isDebugEnabled()) {
-			log.debug("Initiating service...");
-			final Module previous = modules.putIfAbsent(this.primaryKey(), this);
-			log.debug("Registered '{}' is '{}'", this.primaryKey(), Objects.isNull(previous));
-		}
-		runnerFuture = activityRunner.schedule(this::scanModulesHealth, 500, TimeUnit.MILLISECONDS);
-	}
+    /**
+     * Allocate module's resources and get module ready to work
+     */
+    @Override
+    protected void initAsService() {
+        if (log.isDebugEnabled()) {
+            log.debug("Initiating service...");
+        }
+        final Module previous = modules.putIfAbsent(this.primaryKey(), this);
+        if (log.isDebugEnabled()) {
+            log.debug("Registered '{}' is '{}'", this.primaryKey(), Objects.isNull(previous));
+        }
+        runnerFuture = activityRunner.schedule(this::scanModulesHealth, 500, TimeUnit.MILLISECONDS);
+    }
 
-	/**
-	 * Free allocated resources
-	 */
-	@Override
-	protected void shutdownAsService() {
-		log.debug("Stopping service...");
-		if (Objects.nonNull(runnerFuture)) {
-			runnerFuture.cancel(true);
-			runnerFuture = null;
-		}
-		log.debug("Unregistering '{}'", this.primaryKey());
-		modules.remove(this.primaryKey());
-	}
+    /**
+     * Free allocated resources
+     */
+    @Override
+    protected void shutdownAsService() {
+        log.debug("Stopping service...");
+        if (Objects.nonNull(runnerFuture)) {
+            runnerFuture.cancel(true);
+            runnerFuture = null;
+        }
+        log.debug("Unregistering '{}'", this.primaryKey());
+        modules.remove(this.primaryKey());
+    }
 
-	/**
-	 * Notify about changes of configuration property
-	 *
-	 * @param itemName  name of property
-	 * @param itemValue new value of property
-	 */
-	@Override
-	protected boolean configurationItemChanged(String itemName, VariableItem itemValue) {
-		final VariableItem configurationVariable;
-		if (Objects.isNull(configurationVariable = configurationVariableOf(itemName))) {
-			log.warn("No accessible variable '{}' in configuration.", itemName);
-			return false;
-		}
-		// check for delay
-		if (itemName.equals(delayName())) {
-			delay = itemValue.get(Integer.class).longValue();
-			log.debug("Changed variable 'delay' to {}", delay);
-			configurationVariable.set(delay);
-			return true;
-		}
-		// check for ignoredModules
-		if (itemName.equals(ignoreModulesName())) {
-			ignoredModules = itemValue.get(String.class).split(",");
-			log.debug("Changed variable 'ignoredModules' to {}", Arrays.asList(ignoredModules));
-			configurationVariable.set(String.join(",", ignoredModules));
-			return true;
-		}
-		return false;
-	}
+    /**
+     * Notify about changes of configuration property
+     *
+     * @param itemName  name of property
+     * @param itemValue new value of property
+     */
+    @Override
+    protected boolean configurationItemChanged(String itemName, VariableItem itemValue) {
+        final VariableItem configurationVariable;
+        if (Objects.isNull(configurationVariable = configurationVariableOf(itemName))) {
+            log.warn("No accessible variable '{}' in configuration.", itemName);
+            return false;
+        }
+        // check for delay
+        if (itemName.equals(delayName())) {
+            delay = itemValue.get(Integer.class).longValue();
+            log.debug("Changed variable 'delay' to {}", delay);
+            configurationVariable.set(delay);
+            return true;
+        }
+        // check for ignoredModules
+        if (itemName.equals(ignoreModulesName())) {
+            ignoredModules = itemValue.get(String.class).split(",");
+            log.debug("Changed variable 'ignoredModules' to {}", Arrays.asList(ignoredModules));
+            configurationVariable.set(String.join(",", ignoredModules));
+            return true;
+        }
+        return false;
+    }
 
-	/**
-	 * To inspect one module in context of action
-	 *
-	 * @param label  label of module's processing
-	 * @param action action owner of inspection
-	 * @param module module to be inspected
-	 */
-	protected void inspectModule(String label, ModuleAction action, Module module) {
-		final Instant mark = timeService.now();
-		final String modulePK = module.primaryKey();
-		if (Stream.of(ignoredModules).anyMatch(ignorance -> !StringUtils.isEmpty(ignorance) && modulePK.startsWith(ignorance))) {
-			log.debug("Module '{}' is ignored.", modulePK);
-			return;
-		}
-		log.info("Scan module '{}'", modulePK);
-		// get health state of module
-		module.getMetricsContainer().health().heartBeat(action, module);
-		// collect and save all metrics of module
-		AtomicInteger counter = new AtomicInteger(1);
-		module.metrics().stream()
-				.peek(metric -> log.debug("{}. metric '{}' processing", counter.getAndIncrement(), metric.name()))
-				.filter(m -> isActive())
-				.forEach(this::store);
+    /**
+     * To inspect one module in context of action
+     *
+     * @param label  label of module's processing
+     * @param action action owner of inspection
+     * @param module module to be inspected
+     */
+    protected void inspectModule(String label, ModuleAction action, Module module) {
+        final Instant mark = timeService.now();
+        final String modulePK = module.primaryKey();
+        if (Stream.of(ignoredModules).anyMatch(ignorance -> !StringUtils.isEmpty(ignorance) && modulePK.startsWith(ignorance))) {
+            log.debug("Module '{}' is ignored.", modulePK);
+            return;
+        }
+        log.info("Scan module '{}'", modulePK);
+        // get health state of module
+        module.getMetricsContainer().health().heartBeat(action, module);
+        // collect and save all metrics of module
+        AtomicInteger counter = new AtomicInteger(1);
+        module.metrics().stream()
+                .peek(metric -> log.debug("{}. metric '{}' processing", counter.getAndIncrement(), metric.name()))
+                .filter(m -> isActive())
+                .forEach(this::store);
 
-		// save metric about module health check duration
-		getMetricsContainer().duration().simple(label, action, timeService.now(), modulePK, timeService.duration(mark));
-	}
+        // save metric about module health check duration
+        getMetricsContainer().duration().simple(label, action, timeService.now(), modulePK, timeService.duration(mark));
+    }
 
 
-	// private methods
+    // private methods
 
-	/**
-	 * To scan modules registry
-	 */
-	void scanModulesHealth() {
-		// activate main module action
-		activateMainModuleAction();
+    /**
+     * To scan modules registry
+     */
+    void scanModulesHealth() {
+        // activate main module action
+        activateMainModuleAction();
 
-		log.debug("Scanning modules.");
-		actionsFactory.executeAtomicModuleAction(this, "metrics-check", () -> iterateRegisteredModules(ACTIVITY_LABEL), false);
+        log.debug("Scanning modules.");
+        actionsFactory.executeAtomicModuleAction(this, "metrics-check", () -> iterateRegisteredModules(ACTIVITY_LABEL), false);
 
-		if (!isActive() || Objects.isNull(runnerFuture)) {
-			log.debug("Scanning is stopped.");
-			return;
-		}
-		log.debug("Rescheduling service");
-		runnerFuture = activityRunner.schedule(this::scanModulesHealth, delay, TimeUnit.MILLISECONDS);
-	}
+        if (!isActive() || Objects.isNull(runnerFuture)) {
+            log.debug("Scanning is stopped.");
+            return;
+        }
+        log.debug("Rescheduling service");
+        runnerFuture = activityRunner.schedule(this::scanModulesHealth, delay, TimeUnit.MILLISECONDS);
+    }
 
-	/**
-	 * To store metric into metrics storage
-	 *
-	 * @param metric metric to be saved
-	 */
-	void store(ModuleMetric metric) {
-		log.debug("Storing metric '{}' of '{}'", metric.name(), metric.action().getModule().primaryKey());
-		final ModuleAction action = metric.action();
-		final String modulePK = action.getModule().primaryKey();
-		metricStorage.storeMetric(metric.name(), modulePK, metric.measured(), action.getHostName(), metric.valuesAsString());
-		log.debug("Stored '{}'", metric);
-	}
+    /**
+     * To store metric into metrics storage
+     *
+     * @param metric metric to be saved
+     */
+    void store(ModuleMetric metric) {
+        log.debug("Storing metric '{}' of '{}'", metric.name(), metric.action().getModule().primaryKey());
+        final ModuleAction action = metric.action();
+        final String modulePK = action.getModule().primaryKey();
+        metricStorage.storeMetric(metric.name(), modulePK, metric.measured(), action.getHostName(), metric.valuesAsString());
+        log.debug("Stored '{}'", metric);
+    }
 }
--- modularity/core/src/test/java/oleg/sopilnyak/service/registry/impl/HealthModuleServiceTest.java
+++ modularity/core/src/test/java/oleg/sopilnyak/service/registry/impl/HealthModuleServiceTest.java
@@ -1,12 +1,24 @@
 package oleg.sopilnyak.service.registry.impl;
 
+import oleg.sopilnyak.configuration.ModuleSystemConfiguration;
+import oleg.sopilnyak.configuration.ModuleUtilityConfiguration;
 import oleg.sopilnyak.module.Module;
 import oleg.sopilnyak.module.metric.ActionMetricsContainer;
+import oleg.sopilnyak.module.metric.DurationMetricsContainer;
+import oleg.sopilnyak.module.metric.HeartBeatMetricContainer;
 import oleg.sopilnyak.module.metric.MetricsContainer;
+import oleg.sopilnyak.module.metric.storage.ModuleMetricStorage;
+import oleg.sopilnyak.module.model.ModuleAction;
 import oleg.sopilnyak.module.model.action.ModuleActionAdapter;
 import oleg.sopilnyak.module.model.action.ResultModuleAction;
 import oleg.sopilnyak.module.model.action.SuccessModuleAction;
+import oleg.sopilnyak.service.TimeService;
+import oleg.sopilnyak.service.UniqueIdGenerator;
 import oleg.sopilnyak.service.action.ModuleActionFactory;
+import oleg.sopilnyak.service.action.impl.ModuleActionFactoryImpl;
+import oleg.sopilnyak.service.configuration.storage.ModuleConfigurationStorage;
+import oleg.sopilnyak.service.dto.VariableItemDto;
+import oleg.sopilnyak.service.metric.ModuleMetricAdapter;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,9 +27,19 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+
+import static oleg.sopilnyak.service.registry.ModulesRegistryService.*;
+import static org.junit.Assert.*;
 import static org.mockito.Matchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class HealthModuleServiceTest {
@@ -31,7 +53,21 @@ public class HealthModuleServiceTest {
     @Mock
     private ActionMetricsContainer actionMetricsContainer;
     @Mock
-    private ModuleActionFactory actionsFactory;
+    private DurationMetricsContainer durationMetricsContainer;
+    @Mock
+    private HeartBeatMetricContainer heartBeatMetricContainer;
+    @Spy
+    protected ScheduledExecutorService activityRunner = new ModuleUtilityConfiguration().getScheduledExecutorService();
+    @Spy
+    private TimeService timeService = new ModuleUtilityConfiguration().getTimeService();
+    @Spy
+    private UniqueIdGenerator idGenerator = new ModuleUtilityConfiguration().getUniqueIdGenerator();
+    @Spy
+    private ModuleActionFactory actionsFactory = new ModuleSystemConfiguration().getModuleActionFactory();
+    @Mock
+    private ModuleConfigurationStorage configurationStorage;
+    @Mock
+    private ModuleMetricStorage metricStorage;
     @InjectMocks
     private HealthModuleService service = new HealthModuleService();
 
@@ -43,10 +79,14 @@ public class HealthModuleServiceTest {
         when(module.getDescription()).thenReturn("desc-test");
         when(module.primaryKey()).thenReturn("test-pk");
         mainAction = new ModuleActionAdapter(module, "test");
-        when(actionsFactory.createModuleMainAction(any(Module.class))).thenReturn(mainAction);
         ResultModuleAction result = new SuccessModuleAction(mainAction);
-        when(actionsFactory.executeAtomicModuleAction(any(Module.class), anyString(), any(Runnable.class), anyBoolean())).thenReturn(result);
         when(metricsContainer.action()).thenReturn(actionMetricsContainer);
+        when(metricsContainer.duration()).thenReturn(durationMetricsContainer);
+        when(metricsContainer.health()).thenReturn(heartBeatMetricContainer);
+
+        ((ModuleActionFactoryImpl)actionsFactory).setUp();
+        mainAction.setHostName((String) ReflectionTestUtils.getField(actionsFactory, "hostName"));
+        ReflectionTestUtils.setField(actionsFactory, "idGenerator", idGenerator);
 
         service.moduleStart();
     }
@@ -54,50 +94,161 @@ public class HealthModuleServiceTest {
     @After
     public void tearDown() throws Exception {
         service.moduleStop();
+        reset(module, actionsFactory, metricsContainer, actionMetricsContainer);
     }
 
     @Test
     public void testAdd() {
         service.add(module);
+
+        Map<String, Module> modules = (Map<String, Module>) ReflectionTestUtils.getField(service, "modules");
+        assertEquals(module, modules.get(module.primaryKey()));
+        verify(actionsFactory, times(2)).startMainAction(eq(service));
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testAddFail() {
+        service.add(null);
+        fail("Here I'm waiting for an exception.");
     }
 
     @Test
     public void testRemove() {
+        service.add(module);
+
+        Map<String, Module> modules = (Map<String, Module>) ReflectionTestUtils.getField(service, "modules");
+        assertEquals(module, modules.get(module.primaryKey()));
+
+        service.remove(module);
+        assertNull(modules.get(module.primaryKey()));
+
+        verify(actionsFactory, times(3)).startMainAction(eq(service));
     }
 
+    @Test(expected = AssertionError.class)
+    public void testRemoveFail() {
+        service.add(module);
+
+        Map<String, Module> modules = (Map<String, Module>) ReflectionTestUtils.getField(service, "modules");
+        assertEquals(module, modules.get(module.primaryKey()));
+
+        service.remove(null);
+        fail("Here I'm waiting for an exception.");
+    }
     @Test
     public void testRegistered() {
+        service.add(module);
+
+        List<Module> modules = new ArrayList<>(service.registered());
+        assertTrue(modules.contains(module));
     }
 
     @Test
     public void testGetRegistered() {
+        assertNull(service.getRegistered(module));
+
+        service.add(module);
+        assertEquals(module, service.getRegistered(module));
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testGetRegisteredFail() {
+        service.getRegistered((Module)null);
+        fail("Here I'm waiting for an exception.");
     }
 
     @Test
-    public void testGetRegistered1() {
+    public void testGetRegisteredPK() {
+        assertNull(service.getRegistered(module.primaryKey()));
+        service.add(module);
+
+        assertEquals(module, service.getRegistered(module.primaryKey()));
+        assertNull(service.getRegistered((String) null));
+        assertNull(service.getRegistered(""));
     }
 
     @Test
     public void testInitAsService() {
+        service.moduleStop();
+
+        service.initAsService();
+
+        assertEquals(service, service.getRegistered(service));
+        ScheduledFuture runnerFuture = (ScheduledFuture) ReflectionTestUtils.getField(service, "runnerFuture");
+        assertNotNull(runnerFuture);
+        assertFalse(runnerFuture.isDone());
     }
 
     @Test
     public void testShutdownAsService() {
+        service.shutdownAsService();
+
+        assertNull(service.getRegistered(service));
+        assertNull(ReflectionTestUtils.getField(service, "runnerFuture"));
     }
 
     @Test
     public void testConfigurationItemChanged() {
+        Long delay = (Long) ReflectionTestUtils.getField(service, "delay");
+        assertEquals(DELAY_DEFAULT, delay.longValue());
+
+        service.configurationItemChanged(service.delayName(), new VariableItemDto(DELAY_NAME, 5000));
+        delay = (Long) ReflectionTestUtils.getField(service, "delay");
+        assertEquals(5000L, delay.longValue());
+
+        String[] ignoredModules = (String[]) ReflectionTestUtils.getField(service, "ignoredModules");
+        assertEquals(1, ignoredModules.length);
+        assertEquals("", ignoredModules[0]);
+
+        service.configurationItemChanged(service.ignoreModulesName(), new VariableItemDto(IGNORE_MODULE_NAME, "1,2,3"));
+        ignoredModules = (String[]) ReflectionTestUtils.getField(service, "ignoredModules");
+        assertEquals(3, ignoredModules.length);
+        assertEquals("1", ignoredModules[0]);
+        assertEquals("2", ignoredModules[1]);
+        assertEquals("3", ignoredModules[2]);
     }
 
     @Test
     public void testInspectModule() {
+        when(module.getMetricsContainer()).thenReturn(metricsContainer);
+
+        service.inspectModule("test", mainAction, module);
+
+        verify(module, times(1)).primaryKey();
+        verify(module, times(1)).getMetricsContainer();
+        verify(heartBeatMetricContainer, times(1)).heartBeat(eq(mainAction), eq(module));
+        verify(module, times(1)).metrics();
+        verify(durationMetricsContainer, times(1)).simple(eq("test"), eq(mainAction), any(Instant.class), eq("test-pk"), anyLong());
     }
 
     @Test
     public void testScanModulesHealth() {
+        ModuleAction serviceAction = service.getMainAction();
+
+        service.scanModulesHealth();
+
+        verify(actionsFactory, times(2)).startMainAction(eq(service));
+        assertEquals(service, service.getRegistered(service));
+        ScheduledFuture runnerFuture = (ScheduledFuture) ReflectionTestUtils.getField(service, "runnerFuture");
+        assertNotNull(runnerFuture);
+        assertFalse(runnerFuture.isDone());
     }
 
     @Test
     public void store() {
+        ModuleMetricAdapter metric = new ModuleMetricAdapter(mainAction, timeService.now()) {
+            @Override
+            public String name() {
+                return "test-metric";
+            }
+        };
+        service.store(metric);
+        String modulePK = mainAction.getModule().primaryKey();
+        String host = mainAction.getHostName();
+        Instant measured = metric.measured();
+        String data = metric.valuesAsString();
+
+        verify(metricStorage, times(1))
+                .storeMetric(eq("test-metric"), eq(modulePK), eq(measured), eq(host), eq(data));
     }
 }
